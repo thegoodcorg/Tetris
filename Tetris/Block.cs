45f348634b479ec2838098db67cd5af1d9578e17
Jeremy Goodman <jeremygoodman52@gmail.com>
2023-06-29 01:35:21 -0500

    initial commit

---
+++ Tetris/Block.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffset { get; }
+        public abstract int Id { get; }
+        private int rotationState;
+        private Position offset;
+
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Col);
+        }
+
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach (Position p in Tiles[rotationState])
+            {
+                yield return new Position(p.Row + offset.Row, p.Col + offset.Col);
+            }
+        }
+
+        public void RotateCW()
+        {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+
+        public void RotateCCW()
+        {
+            if (rotationState == 0)
+            {
+                rotationState = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationState--;
+            }
+        }
+
+        public void Move(int rows, int cols)
+        {
+            offset.Row = rows;
+            offset.Col = cols;
+        }
+
+        public void Reset()
+        {
+            rotationState = 0;
+            offset.Row = StartOffset.Row;
+            offset.Col = StartOffset.Col;
+        }
+    }
+}
---
+++ Tetris/GameState.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class GameState
+    {
+        private Block currentBlock;
+
+        public Block CurrentBlock
+        {
+            get => currentBlock;
+            private set
+            {
+                currentBlock = value;
+                currentBlock.Reset();
+            }
+        }
+
+        public GameGrid GameGrid { get; }
+        public BlockQueue BlockQueue { get; }
+        public bool GameOver { get; private set; }
+
+        public GameState()
+        {
+            GameGrid = new GameGrid(22, 10);
+            BlockQueue = new BlockQueue();
+            CurrentBlock = BlockQueue.GetAndUpdate();
+        }
+
+        private bool BlockFits()
+        {
+            foreach (Position p in CurrentBlock.TilePositions())
+            {
+                if (!GameGrid.IsEmpty(p.Row, p.Col))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void RotateBlockCW()
+        {
+            currentBlock.RotateCW();
+
+            if (!BlockFits())
+            {
+                currentBlock.RotateCCW();
+            }
+        }
+
+        public void RotateCCW()
+        {
+            currentBlock.RotateCCW();
+
+            if(!BlockFits())
+            {
+                currentBlock.RotateCW();
+            }
+        }
+
+        public void MoveBlockLeft()
+        {
+            currentBlock.Move(0, -1);
+
+            if (!BlockFits())
+            {
+                currentBlock.Move(0, 1);
+            }
+        }
+
+        public void MoveBlockright()
+        {
+            currentBlock.Move(0, 1);
+
+            if(!BlockFits())
+            {
+                currentBlock.Move(0, -1);
+            }
+        }
+
+        private bool IsGameOver()
+        {
+            return !(GameGrid.IsEmpty(0)  && !GameGrid.IsEmpty(1));
+        }
+
+        private void Placeblock()
+        {
+            foreach(Position p in CurrentBlock.TilePositions())
+            {
+                GameGrid[p.Row, p.Col] = currentBlock.Id;
+            }
+
+            GameGrid.ClearFullRows();
+
+            if(IsGameOver())
+            {
+                GameOver =  true;
+            }
+            else
+            {
+                CurrentBlock = BlockQueue.GetAndUpdate();
+            }
+        }
+
+        public void MoveBlockDown()
+        {
+            CurrentBlock.Move(1,0);
+
+            if (!BlockFits())
+            {
+                CurrentBlock.Move(-1,0);
+                Placeblock();
+            }
+        }
+    }
+}
